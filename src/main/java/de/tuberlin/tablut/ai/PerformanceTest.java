66af7b9ea90961074509262507888ed081b2f9c2
kruegerhash <landolf.kr2@gmail.com>
2026-04-24 14:52:51 +0200

    feat: add performance test
    Signed-off-by: kruegerhash <landolf.kr2@gmail.com>

---
+++ src/main/java/de/tuberlin/tablut/ai/PerformanceTest.java
@@ -0,0 +1,32 @@
+package de.tuberlin.tablut.ai;
+
+import java.util.ArrayList;
+
+public class PerformanceTest {
+    public static void main(String[] args){
+        Board base = new Board();
+        System.out.println(perft(base, 1, Player.BLACK));
+    }
+    // Without piece hit
+    private static int perft(Board board, int depth, Player player){
+        if(depth == 0) return 1;
+
+        // generate all possible moves
+        ArrayList<Move> moves = Board.generateLegalMoves(board, player);
+
+        int nodes = 0;
+        // iterate through all moves
+        for(Move move : moves){
+            // make a deep copy of the board
+            Board copy = Board.deepCopy(board);
+
+            // apply the moves
+            copy.applyMove(move);
+
+            // call perft recursively
+            nodes += perft(copy, depth-1, Board.oppositeSide(player));
+        }
+        return nodes;
+
+    }
+}
