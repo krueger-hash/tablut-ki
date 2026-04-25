f3ad0f2e14e0a9a207f0478d5bd28714a2661fde
velocht <147926794+velocht@users.noreply.github.com>
2026-04-25 15:59:51 +0200

    feat: toString-Methode und Methode für Umwandlung von Move Objekt in int Array mit Koordinaten

--- src/main/java/de/tuberlin/tablut/ai/Move.java
+++ src/main/java/de/tuberlin/tablut/ai/Move.java
@@ -1,5 +1,7 @@
 package de.tuberlin.tablut.ai;
 
+import java.util.Arrays;
+
 public class Move {
 
     //Das spielfeld wird einfach durchnummeriert, entsprechend den Bits, also von 0 bis 89 von oben links nach unten rechts
@@ -38,6 +40,35 @@ public class Move {
         }
 
         return new Move(fromX,fromY,toX,toY,moved); // hier ist der alternative Konstruktor genutzt
+    }
 
+    // gibt Ursprung und Ziel des Moves als einen Int Array zur
+    static int[] moveToIndizes(Move move){
+        int[] fromCoords = Bitboard90.bitToMatrix(move.from);
+        int[] toCoords = Bitboard90.bitToMatrix(move.to);
+        int fromRow = fromCoords[0];
+        int fromCol = fromCoords[1];
+        int toRow = toCoords[0];
+        int toCol = toCoords[1];
+        return new int[]{fromRow,fromCol,toRow,toCol};
+    }
+
+    @Override
+    public String toString() {
+        String moved;
+        if (this.movedPiece == Piece.BLACK){moved ="b";}
+        else if (this.movedPiece == Piece.WHITE){moved="w";}
+        else if (this.movedPiece == Piece.KING){moved="K";}
+        else {throw new IllegalArgumentException("Something wrong with movedPiece - called with:"+this.movedPiece);}
+
+        String origin = Arrays.toString(Bitboard90.bitToMatrix(this.from));
+        String target = Arrays.toString(Bitboard90.bitToMatrix(this.from));
+
+        return "Move{"
+                + moved +" "
+                + origin
+                +"->"
+                + target
+                +"}";
     }
 }
