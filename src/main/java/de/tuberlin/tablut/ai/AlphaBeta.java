630a8528726fc1685500c1c2b3e3364a30bc7e76
velocht <147926794+velocht@users.noreply.github.com>
2026-05-06 10:13:58 +0200

    feat: Überarbeitung der AB-Suchen, um dem Pseudocode aus der GKI-VL zu entsprechen
    Ich vertraue darauf, dass der klappt. Zumindest ist mir damals kein Fehler aufgefallen.

--- src/main/java/de/tuberlin/tablut/ai/AlphaBeta.java
+++ src/main/java/de/tuberlin/tablut/ai/AlphaBeta.java
@@ -4,39 +4,44 @@ import java.util.ArrayList;
 
 public class AlphaBeta {
 
+    //nur Mock-Up, damit ich nicht die ganze Zeit rote Linien sehen muss
     static int evaluateState(Board state){
         return 0;
     }
 
+    //Wrapper für den Aufruf der Alpha-Beta-Suche. Alternativ hätte man auch alles in eine Funktion mit Fallunterscheidung reinpacken können. Ich fand aber die Zweiteilung in Min-Max aber besser, da analog zur GKI-VL
     static int alphaBetaSearch(Board state, int depth, int alpha, int beta){
-        if (state.sideToMove == Player.WHITE){
+        if (state.sideToMove == BestMove.maxPlayer){
             return alphaBetaMax(state,depth,alpha,beta);
         }
-        if (state.sideToMove == Player.BLACK){
+        if (state.sideToMove == BestMove.minPlayer){
             return alphaBetaMin(state,depth,alpha,beta);
         }
         else {
-            throw new IllegalArgumentException("Kein Zugspieler definiert!");
+            throw new IllegalStateException("Übergebenes Board hat ungültige .sideToMove");
         }
     }
 
+    //Alpha-Beta-Min basierend auf GKI-VL WS2526 05A01 Folie 14
     static int alphaBetaMax(Board state, int depth, int alpha, int beta){
         if(depth == 0 || state.gameIsEnd()){
             return evaluateState(state);
         }
-        int value = -9001;
         ArrayList<Move> moves = Board.generateLegalMoves(state, state.sideToMove);
         for (Move move : moves){
             Board newState = Board.boardAfterMove(state, move);
-            value = Math.max(value,alphaBetaMin(newState,depth-1,alpha,beta)); //korrekt? hier max von value und AB?
-            alpha = Math.max(alpha, value); // geht das auch kürzer?
-            if (alpha >= beta){ // Bedingung korrekt?
-                break; //Cutoff
+            int score = alphaBetaMin(newState,depth-1,alpha,beta); // Aufruf von ABMin
+            if (score >= beta){
+                return beta; //Cutoff
+            }
+            if (score > alpha){
+                alpha = score;
             }
         }
-        return value; // return korrekt?
+        return alpha;
     }
 
+    //Alpha-Beta-Max basierend auf GKI-VL WS2526 05A01 Folie 14
     static int alphaBetaMin(Board state, int depth, int alpha, int beta){
         if(depth == 0 || state.gameIsEnd()){
             return evaluateState(state);
@@ -45,12 +50,14 @@ public class AlphaBeta {
         ArrayList<Move> moves = Board.generateLegalMoves(state, state.sideToMove);
         for (Move move : moves){
             Board newState = Board.boardAfterMove(state, move);
-            value = Math.min(value,alphaBetaMax(newState,depth-1,alpha,beta)); //korrekt? hier max von value und AB?
-            alpha = Math.min(alpha, value); // geht das auch kürzer?
-            if (beta <= alpha){ // Bedingung korrekt?
-                break; //Cutoff
+            int score = alphaBetaMax(newState,depth-1,alpha,beta); //Aufruf von ABMax
+            if (score <= alpha){
+                return alpha; //Cutoff
+            }
+            if (score < beta){
+                beta = score;
             }
         }
-        return value; // return korrekt?
+        return beta;
     }
 }
