a00cb05228ff07fd766cd2ff3be54d88cdbbd5dd
velocht <147926794+velocht@users.noreply.github.com>
2026-05-07 15:37:08 +0200

    fix: Änderungen aus der Besprechung

--- src/main/java/de/tuberlin/tablut/ai/BestMove.java
+++ src/main/java/de/tuberlin/tablut/ai/BestMove.java
@@ -9,22 +9,25 @@ public class BestMove {
     static final Player minPlayer = Player.BLACK;
 
 
-    //Achtung: gerade nur für einen Spieler gültig, da value nur maximiert wird!!
-
-    static Move getBestMove(Board state,int depth){ // Übergabe von Parametern muss noch angepasst werden auf unterschiedliche Fälle; Ich würde hier in dieser Funktion die Fallunterscheidungen machen wollen. Vllt ein Parameter-Objekt übergeben?
-        int bestValue = 0;
-        Move bestMove = null;
-
+    // Übergabe von Parametern muss noch angepasst werden auf unterschiedliche Fälle; Ich würde hier in dieser Funktion die Fallunterscheidungen machen wollen. Vllt ein Parameter-Objekt übergeben? <~~ bezüglich anderer Suchstrategien/Modularisierung
+    static Move getBestMove(Board state,int depth){
         ArrayList<Move> moves = Board.generateLegalMoves(state, state.sideToMove);
+        int bestValue = 0;
+        Move bestMove = moves.getFirst();
+
         for (Move move : moves){
-            Board newState = Board.boardAfterMove(state, move);
+            Board newState = Board.boardAfterMove(state, move); // Refactor auf makeMove
+            // Aufruf des Alpha-Beta-Fensters ist typischerweise mit +/- unendlich.
             int value = AlphaBeta.alphaBetaSearch(newState,depth,-9001,9001);
+
+            // hier ggf mit unmake Move
             if(state.sideToMove == BestMove.maxPlayer) {
                 if (value > bestValue) {
                     bestValue = value;
                     bestMove = move;
                 }
-            } else if (state.sideToMove == BestMove.minPlayer) {
+            }
+            else if (state.sideToMove == BestMove.minPlayer) {
                 if (value < bestValue) {
                     bestValue = value;
                     bestMove = move;
