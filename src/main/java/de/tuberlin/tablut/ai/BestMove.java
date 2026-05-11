869170aa9f0e95c2a4bfc354613f284ab35aa370
velocht <147926794+velocht@users.noreply.github.com>
2026-05-12 00:23:11 +0200

    feat: umstellen von BoardAfterMove auf make/unmake Move

--- src/main/java/de/tuberlin/tablut/ai/BestMove.java
+++ src/main/java/de/tuberlin/tablut/ai/BestMove.java
@@ -8,21 +8,18 @@ public class BestMove {
     private static final Player maxPlayer = BoardEvaluator.MAX_PLAYER;
     private static final Player minPlayer = BoardEvaluator.MIN_PLAYER;
 
-    //erstmal hier eine globale Definition für min bzw. max spieler; vmtl besser in Bewertungsfunktion
-
-
-    // Übergabe von Parametern muss noch angepasst werden auf unterschiedliche Fälle; Ich würde hier in dieser Funktion die Fallunterscheidungen machen wollen. Vllt ein Parameter-Objekt übergeben? <~~ bezüglich anderer Suchstrategien/Modularisierung
     static Move getBestMove(Board state,int depth){
         ArrayList<Move> moves = Board.generateLegalMoves(state, state.sideToMove);
         int bestValue = 0;
         Move bestMove = moves.getFirst();
 
         for (Move move : moves){
-            Board newState = Board.boardAfterMove(state, move); // Refactor auf makeMove
-            // Aufruf des Alpha-Beta-Fensters ist typischerweise mit +/- unendlich.
-            int value = AlphaBeta.alphaBetaSearch(newState,depth,-9001,9001);
 
-            // hier ggf mit unmake Move
+            state.makeMove(move);
+            // Aufruf des Alpha-Beta-Fensters ist typischerweise mit +/- unendlich.
+            int value = AlphaBeta.alphaBetaSearch(state,depth,-9001,9001);
+            state.unmakeMove();
+
             if(state.sideToMove == maxPlayer) {
                 if (value > bestValue) {
                     bestValue = value;
