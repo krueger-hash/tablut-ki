d44b16dac1a70b9d607a0c09e915dc8f48ab5def
velocht <147926794+velocht@users.noreply.github.com>
2026-05-12 12:23:30 +0200

    fix: schweren Fehler bei Zugbewertung über Iterationen behoben; nur das Ergebnis von vollständiger Ebene wird zurückgegeben

--- src/main/java/de/tuberlin/tablut/ai/BestMoveInTime.java
+++ src/main/java/de/tuberlin/tablut/ai/BestMoveInTime.java
@@ -13,7 +13,8 @@ public class BestMoveInTime{
 
 
     private volatile Move bestMove; //volatile Variable, damit Future sie überschreiben kann und ein Ergebnis auch bei Timeout geliefert wird
-    private volatile int bestValue;
+    private volatile Move bestMoveDuringIteration;
+    private volatile int bestValueDuringIteration;
     private final CompletableFuture<Move> future;
 
     BestMoveInTime(Board state, int msTime) {
@@ -23,14 +24,19 @@ public class BestMoveInTime{
 
             // * Logik von BestMove
             ArrayList<Move> moves = Board.generateLegalMoves(state, state.sideToMove);
-            this.bestValue = 0;
             this.bestMove = moves.getFirst(); // der erste Move erstmal als Ausgangspunkt
 
             //iterative Tiefensuche
             for (int depth = 0; ; depth++) {
+
+                this.bestMoveDuringIteration = this.bestMove;
+                this.bestValueDuringIteration = 0; // bestValue muss auf jeder Suchtiefe neu initialisiert werden, da ggf. bei größerer Tiefe identische Züge schlechter bewertet werden können, als mit geringerer Tiefe
+
                 long iterationStart = System.currentTimeMillis();
                 this.bestMoveAtDepth(state, moves, depth);
                 long iterationEnd = System.currentTimeMillis();
+
+                this.bestMove = this.bestMoveDuringIteration;
 
                 //Abbruchbedingung sinnvoll?
                 long remainingTime = msTime - (iterationEnd - tStart);
@@ -39,7 +45,7 @@ public class BestMoveInTime{
 
             }
 
-            return bestMove;
+            return this.bestMove;
         }).orTimeout(msTime,TimeUnit.MILLISECONDS);
     }
 
@@ -60,22 +66,25 @@ public class BestMoveInTime{
 
         //ungetestet :/
     }
+
+    // während einer Suchtiefe wird der beste Move auf der Iterationsvariable gespeichert, damit bestMove nur basierend auf einer vollständig durchsuchten Ebene zurückgegeben wird;
+    //bestValue benötigt keine intermediate Variable
     void bestMoveAtDepth(Board state, ArrayList<Move> moves, int depth){
         for (Move move : moves){
             state.makeMove(move);
-            int value = AlphaBeta.alphaBetaSearch(state,depth,-9001,9001); // Aufruf des Alpha-Beta-Fensters ist typischerweise mit +/- unendlich, ~ 9000+ ist äquivalent
+            int value = AlphaBeta.sortedAlphaBetaSearch(state,depth,-9001,9001); // Aufruf des Alpha-Beta-Fensters ist typischerweise mit +/- unendlich, ~ 9000+ ist äquivalent
             state.unmakeMove();
 
             if(state.sideToMove == maxPlayer) {
-                if (value > bestValue) {
-                    this.bestValue = value;
-                    this.bestMove = move;
+                if (value > this.bestValueDuringIteration) {
+                    this.bestValueDuringIteration = value;
+                    this.bestMoveDuringIteration = move;
                 }
             }
             else if (state.sideToMove == minPlayer) {
-                if (value < bestValue) {
-                    this.bestValue = value;
-                    this.bestMove = move;
+                if (value < this.bestValueDuringIteration) {
+                    this.bestValueDuringIteration = value;
+                    this.bestMoveDuringIteration = move;
                 }
             }
             else {throw new IllegalStateException("Übergebenes Board hat ungültige .sideToMove");}
