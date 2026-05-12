b1852e4646640fb48a71c56af2a544054a255989
velocht <147926794+velocht@users.noreply.github.com>
2026-05-12 13:17:29 +0200

    feat: zu kleines Suchintervall behoben und  zwischenValue-Speicherung implementiert

--- src/main/java/de/tuberlin/tablut/ai/BestMoveInTime.java
+++ src/main/java/de/tuberlin/tablut/ai/BestMoveInTime.java
@@ -13,8 +13,11 @@ public class BestMoveInTime{
 
 
     private volatile Move bestMove; //volatile Variable, damit Future sie überschreiben kann und ein Ergebnis auch bei Timeout geliefert wird
+    private volatile int bestValue;
+
     private volatile Move bestMoveDuringIteration;
     private volatile int bestValueDuringIteration;
+
     private final CompletableFuture<Move> future;
 
     BestMoveInTime(Board state, int msTime) {
@@ -24,7 +27,8 @@ public class BestMoveInTime{
 
             // * Logik von BestMove
             ArrayList<Move> moves = Board.generateLegalMoves(state, state.sideToMove);
-            this.bestMove = moves.getFirst(); // der erste Move erstmal als Ausgangspunkt
+            this.bestMove = moves.getFirst();
+            this.bestValue = 0;// der erste Move erstmal als Ausgangspunkt
 
             //iterative Tiefensuche
             for (int depth = 0; ; depth++) {
@@ -37,6 +41,7 @@ public class BestMoveInTime{
                 long iterationEnd = System.currentTimeMillis();
 
                 this.bestMove = this.bestMoveDuringIteration;
+                this.bestValue = this.bestValueDuringIteration;
 
                 //Abbruchbedingung sinnvoll?
                 long remainingTime = msTime - (iterationEnd - tStart);
@@ -57,6 +62,13 @@ public class BestMoveInTime{
         }
     }
 
+    public int getBestValueDuringIteration() {
+        return bestValueDuringIteration;
+    }
+    public Move getBestMoveDuringIteration() {
+        return bestMoveDuringIteration;
+    }
+
     //Illustration der Anwendung
     static void main() {
         String fen ="3bbb3/4b4/4w4/b3w3b/bbwwKwwbb/b3w3b/4w4/4b4/3bbb3 S 48";
@@ -72,7 +84,7 @@ public class BestMoveInTime{
     void bestMoveAtDepth(Board state, ArrayList<Move> moves, int depth){
         for (Move move : moves){
             state.makeMove(move);
-            int value = AlphaBeta.sortedAlphaBetaSearch(state,depth,-9001,9001); // Aufruf des Alpha-Beta-Fensters ist typischerweise mit +/- unendlich, ~ 9000+ ist äquivalent
+            int value = AlphaBeta.sortedAlphaBetaSearch(state,depth,-1_000_000,1_000_000); // Aufruf des Alpha-Beta-Fensters ist typischerweise mit +/- unendlich
             state.unmakeMove();
 
             if(state.sideToMove == maxPlayer) {
